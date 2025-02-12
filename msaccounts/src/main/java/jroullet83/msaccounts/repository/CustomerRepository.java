3ac70ca6c61bfce6346bf1c249ee8660b8c24c80
Jacques <jacques.roullet@laposte.net>
2025-02-12 09:15:35 +0100

    First Commit - H2 db, controller my-account issues

---
+++ msaccounts/src/main/java/jroullet83/msaccounts/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package jroullet83.msaccounts.repository;
+
+import jroullet83.msaccounts.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
