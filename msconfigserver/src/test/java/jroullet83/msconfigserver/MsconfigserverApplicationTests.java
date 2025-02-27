0bce2e91e85fd79bb168cfcfec44afcb5381360c
Jacques <jacques.roullet@laposte.net>
2025-02-27 13:26:07 +0100

    Feature : Docker images - msconfigserver - before environment variables management

--- msconfigserver/src/test/java/jroullet83/msconfigserver/MsconfigserverApplicationTests.java
+++ msconfigserver/src/test/java/jroullet83/msconfigserver/MsconfigserverApplicationTests.java
@@ -2,6 +2,8 @@ package jroullet83.msconfigserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
 class MsconfigserverApplicationTests {
